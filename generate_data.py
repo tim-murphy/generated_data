9853d629a7835f84381be7a7ac971b0091820d32
Tim Murphy <tim.murphy@canberra.edu.au>
2026-09-17 23:44:13 +1000

    more functionality

--- generate_data.py
+++ generate_data.py
@@ -22,10 +22,18 @@ if __name__ == '__main__':
                         help="Add noise to the data with this standard deviation")
     parser.add_argument("--round", type=float, required=False,
                         help="If set, will round values to this number")
+    parser.add_argument("--separator", type=str, default="\n",
+                        help="String printed between values (defaults to newline)")
+    parser.add_argument("--repeats", type=int, default=1,
+                        help="Number of times to generate the sequence, separated by a newline")
+    parser.add_argument("--variability", type=float, default=2.0,
+                        help="Standard deviation of variance between repeats")
+    parser.add_argument("--measures", type=int, default=1,
+                        help="Number of measures for each measurement event (default 1)")
     args = parser.parse_args()
 
     # Validate command-line arguments.
-    if args.min >= args.max:
+    if args.min > args.max:
         raise ValueError("Min x value must be less than max x value!")
 
     num_steps = (args.max - args.min) / args.step
@@ -36,14 +44,24 @@ if __name__ == '__main__':
     # Generate the values.
     calc_fn = sympy.lambdify(sympy.symbols('x'),
                              sympy.parse_expr(args.formula))
-    noise = np.random.normal(0.0, args.noise_sd, num_steps)
-    for s in range(num_steps):
-        x = args.min + (s * args.step)
-        y = calc_fn(x) + noise[s]
 
-        if args.round is not None:
-            y = round(y / args.round) * args.round
+    variability = np.random.normal(0.0, args.variability, args.repeats)
 
-        print(y)
+    for rep in range(args.repeats):
+        noise = np.random.normal(0.0, args.noise_sd, num_steps * args.measures)
+        y_vals = []
+        for s in range(num_steps):
+            x = args.min + (s * args.step)
+            y = calc_fn(x) + variability[rep]
+
+            for m in range(args.measures):
+                y_val = y +  + noise[s * args.measures + m]
+
+                if args.round is not None:
+                    y_val = round(y_val / args.round) * args.round
+
+                y_vals.append(y_val)
+
+        print(*y_vals, sep=args.separator)
 
 # EOF
